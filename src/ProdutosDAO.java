3426183eb0d6d8b21d369d3af45fe1d52cfcb9eb
rgiz <regisramalhosilva@gmail.com>
2023-07-15 16:39:22 -0300

    botão de salvar cadastrando o produto

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -7,36 +7,62 @@
  *
  * @author Adm
  */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+
+    public boolean conectar() { // método que vai fazer a conexão com o banco de dados 
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uc11?useSSL=false", "root", "81397848");
+            System.out.println("Conectado");
+            return true;
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("Erro ao conectar: " + ex.getMessage());
+            return false;
+        }
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
+
+    public void desconectar() { // método que vai desconectar do banco de dados 
+        try {
+            conn.close();
+            System.out.println("Desconectado!");
+        } catch (SQLException ex) {
+
+        }
+    }
+
+    public void cadastrarProduto(ProdutosDTO produto) {
         
+        try {
+            prep = conn.prepareStatement("INSERT INTO produtos VALUES(0,?,?,?)");
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            prep.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Produto Cadastrado");
+
+        } catch (SQLException ex) {
+            System.out.println("Erro ao conectar: " + ex.getMessage());
+            JOptionPane.showMessageDialog(null, "Produto Não Cadastrado");
+
+        }
+    }
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
         return listagem;
     }
-    
-    
-    
-        
-}
 
+}
